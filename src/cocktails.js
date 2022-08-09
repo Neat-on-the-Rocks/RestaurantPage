8938a291d8ed1c19f21640513842af00a57c0f41
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2022-08-09 15:11:52 -0500

    Import all images in src/cocktails

--- src/cocktails.js
+++ src/cocktails.js
@@ -1,3 +1,14 @@
+import oldFashioned from './cocktails/oldFashioned.png';
+import newYorkSour from './cocktails/newYorkSour.jpg';
+import appleTini from './cocktails/appletini.jpg';
+import espressoMartini from './cocktails/espressoMartini.jpg';
+import mojito from './cocktails/mojito.jpg';
+import jamaicanParadise from './cocktails/jamaicanParadise.jpg';
+import negroni from './cocktails/negroni.jpg';
+import singaporeSling from './cocktails/singaporeSling.jpg';
+import caddilacMargarita from './cocktails/caddilacMargarita.jpg';
+import tequilaSunrise from './cocktails/tequilaSunrise.webp';
+
 
 export function cocktails() {
     const content = document.createElement('div');
