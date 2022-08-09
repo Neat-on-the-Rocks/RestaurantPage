835cceac9fd7d32f6a4e35e7773fbfcf8f92a64e
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2022-08-09 17:07:50 -0500

    Style about page

--- src/about.js
+++ src/about.js
@@ -2,14 +2,14 @@
 
 export function about() {
     const content = document.createElement('div');
-    content.classList.add('content');
+    content.classList.add('about-content');
 
     const aboutHeader = document.createElement('h2');
     const aboutParagraph = document.createElement('p')
 
     aboutHeader.textContent = "Welcome to Neat on the Rocks"
     aboutParagraph.textContent = "This website is an online cocktail bar. If you navigate to our Cocktails tab you will find a list of our cocktails. In future updates, you will find an option to buy everything needed for the cocktails with a total; as well as, the tools needed. Join us in the journey to make more home bars and better bartenders."
-    content.textContent = "Content Works: About"
+
     content.appendChild(aboutHeader);
     content.appendChild(aboutParagraph);
 
