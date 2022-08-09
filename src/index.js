3f0bad1a756595e28f0c9c445d47ee7446659cc2
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2022-08-09 13:43:10 -0500

    Make image display on page

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import './style.css';
+import Logo from './oldFashioned.png'
 
 function component() {
     const content = document.createElement('div');
     content.classList.add('content');
-
+    
+    const myLogo = new Image();
+    myLogo.src = Logo;
+  
     const header = document.createElement('h1');
     header.innerHTML = "Neat on the Rocks"
-    content.innerHTML = "hello"
-  
+
+    content.appendChild(myLogo);
     content.appendChild(header);
     return content;
 
