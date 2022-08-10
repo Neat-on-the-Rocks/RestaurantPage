a49572a45c3d90026cdb26b2677724a7dd593aad
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2022-08-10 13:40:15 -0500

    Set header and about as initial load functions

--- src/index.js
+++ src/index.js
@@ -1,29 +1,6 @@
+import { about } from './about';
+import { header } from './header';
 import './style.css';
-import {header} from './header.js'
-import {about} from './about.js'
-import {cocktails} from './cocktails'
-import {contact} from './contact'
 
-
-const firstLoad = () =>{
-    document.body.appendChild(header());
-    document.body.appendChild(about());
-}
-
-
-const aboutLoad = () => {
-    document.body.appendChild(header());
-    document.body.appendChild(about());
-}
-
-const cocktailLoad = () => {
-    document.body.appendChild(header());
-    document.body.appendChild(cocktails());
-}
-
-const contactLoad = () => {
-    document.body.appendChild(header());
-    document.body.appendChild(contact());
-}
-
-firstLoad();
+document.body.appendChild(header());
+document.body.appendChild(about());
