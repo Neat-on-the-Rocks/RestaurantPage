cdddbe9b180457976c053204d069770868e97d11
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2022-08-10 12:56:23 -0500

    Create Nav Bar

--- src/header.js
+++ src/header.js
@@ -1,5 +1,27 @@
 import Logo from './oldFashioned.png'
 
+function createNav() {
+    const nav = document.createElement("nav");
+  
+    const homeButton = document.createElement("button");
+    homeButton.classList.add("button-nav");
+    homeButton.textContent = "About";
+    
+    const menuButton = document.createElement("button");
+    menuButton.classList.add("button-nav");
+    menuButton.textContent = "Cocktails";
+  
+    const contactButton = document.createElement("button");
+    contactButton.classList.add("button-nav");
+    contactButton.textContent = "Contact";
+  
+    nav.appendChild(homeButton);
+    nav.appendChild(menuButton);
+    nav.appendChild(contactButton);
+  
+    return nav;
+  }
+
 export function header() {
     const header = document.createElement('div');
     header.classList.add('header');
@@ -12,6 +34,7 @@ export function header() {
 
     header.appendChild(myLogo);
     header.appendChild(title);
+    header.appendChild(createNav());
     return header;
 
 }
