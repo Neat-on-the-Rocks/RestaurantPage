fe58674413556a8853caa32335bddef04760eee2
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2022-08-10 13:06:15 -0500

    Create two divs one for left header and one for right for styling

--- src/header.js
+++ src/header.js
@@ -1,12 +1,50 @@
 import Logo from './oldFashioned.png'
 
-function createNav() {
+// function createNav() {
+//     const nav = document.createElement("nav");
+  
+//     const homeButton = document.createElement("button");
+//     homeButton.classList.add("button-nav");
+//     homeButton.textContent = "About";
+
+//     const menuButton = document.createElement("button");
+//     menuButton.classList.add("button-nav");
+//     menuButton.textContent = "Cocktails";
+  
+//     const contactButton = document.createElement("button");
+//     contactButton.classList.add("button-nav");
+//     contactButton.textContent = "Contact";
+  
+//     nav.appendChild(homeButton);
+//     nav.appendChild(menuButton);
+//     nav.appendChild(contactButton);
+  
+//     return nav;
+//   }
+
+function leftHeader() {
+    const headerLeft = document.createElement('div');
+    headerLeft.classList.add('header-left');
+    
+    const myLogo = new Image();
+    myLogo.src = Logo;
+  
+    const title = document.createElement('h1');
+    title.innerHTML = "Neat on the Rocks";
+
+    headerLeft.appendChild(myLogo);
+    headerLeft.appendChild(title);
+
+    return headerLeft;
+}
+
+function rightHeader() {
     const nav = document.createElement("nav");
   
     const homeButton = document.createElement("button");
     homeButton.classList.add("button-nav");
     homeButton.textContent = "About";
-    
+
     const menuButton = document.createElement("button");
     menuButton.classList.add("button-nav");
     menuButton.textContent = "Cocktails";
@@ -20,21 +58,15 @@ function createNav() {
     nav.appendChild(contactButton);
   
     return nav;
-  }
+}
 
 export function header() {
     const header = document.createElement('div');
     header.classList.add('header');
     
-    const myLogo = new Image();
-    myLogo.src = Logo;
-  
-    const title = document.createElement('h1');
-    title.innerHTML = "Neat on the Rocks"
+    header.appendChild(leftHeader());
+    header.appendChild(rightHeader());    
 
-    header.appendChild(myLogo);
-    header.appendChild(title);
-    header.appendChild(createNav());
     return header;
 
 }
